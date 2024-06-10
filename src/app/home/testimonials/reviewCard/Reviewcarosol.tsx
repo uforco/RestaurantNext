a2c2e7773e3dd62cddec062c9c57e5881b0cd5f2
sharif <srka780@gmail.com>
2024-06-11 00:39:31 +0600

    Testimonials carusol auto move system

--- src/app/home/testimonials/reviewCard/Reviewcarosol.tsx
+++ src/app/home/testimonials/reviewCard/Reviewcarosol.tsx
@@ -5,15 +5,47 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
 const Reviewcarosol = () => {
-  const [sliderRef] = useKeenSlider({
-    mode: "free-snap",
-    loop: true,
-    slides: {
-      origin: "center",
-      perView: 1,
-      spacing: 15,
+  const [sliderRef] = useKeenSlider(
+    {
+      mode: "free-snap",
+      loop: true,
+      slides: {
+        origin: "center",
+        perView: 1,
+        spacing: 15,
+      },
     },
-  });
+    [
+      (slider) => {
+        let timeout: any;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 2000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
+  );
   return (
     <div ref={sliderRef} className="keen-slider w-full">
       <div className="keen-slider__slide py-14 number-slide1">
